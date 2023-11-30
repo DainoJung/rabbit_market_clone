016fd933041d1365b14727cfdc055cae35e43c6d
DainJung <wjdekdls3693@gmail.com>
2023-11-30 10:42:53 +0900

    extension

--- lib/screen/post_detail/provider/product_post_provider.dart
+++ lib/screen/post_detail/provider/product_post_provider.dart
@@ -1,8 +1,11 @@
+import 'package:fast_app_base/common/dart/extension/num_duration_extension.dart';
+import 'package:fast_app_base/common/dart/extension/ref_extension.dart';
 import 'package:fast_app_base/data/network/daangn_api.dart';
 import 'package:fast_app_base/entity/post/vo_product_post.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 final productPostProvider =
     AutoDisposeFutureProviderFamily<ProductPost, int>((ref, id) async {
+  ref.cacheFor(10.minutes);
   return await DaangnApi.getPost(id);
 });
